460323dbcc1a7a9cfc2b92f02dd72ee2852ec37a
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-20 19:53:46 +0200

    Prevent embedded aliases from running browser hooks twice

--- mycompany.js
+++ mycompany.js
@@ -1,3 +1,18 @@
 "use strict";
 
-module.exports.mycompany = require("./plugin.js").mycompany;
+var createMyCompany = require("./plugin.js").mycompany;
+
+module.exports.mycompany = function (parent) {
+    var obj = createMyCompany(parent);
+
+    // Embedded modules are registered in parent.plugins only so backend modules
+    // can discover each other. Their browser hooks are exported by MyCompany
+    // under embedded*Startup names and must not be invoked a second time by the
+    // MeshCentral plugin handler before MyCompanyAssetUrl exists.
+    ["myscripts", "mycommands", "approvalcenter", "moverequest"].forEach(function (shortName) {
+        if (parent && parent.exports) parent.exports[shortName] = [];
+        if (parent && parent.plugins && parent.plugins[shortName]) parent.plugins[shortName].exports = [];
+    });
+
+    return obj;
+};
