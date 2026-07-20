38e466f3b925d330d7c0231075a1ce1c78c20eee
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-20 17:37:20 +0200

    Use module-local Files directory for My Scripts sources

--- modules/scripts/index.js
+++ modules/scripts/index.js
@@ -1,10 +1,23 @@
 "use strict";
 
+var fs = require("fs");
+var path = require("path");
+
 exports.create = function (runtime) {
+    var moduleRoot = __dirname;
+    var filesRoot = path.join(moduleRoot, "Files");
+    var legacyRoot = path.resolve(moduleRoot, "..", "..", "legacy", "myscripts");
+    var sourceRoot = fs.existsSync(path.join(filesRoot, "package.json")) ? filesRoot : legacyRoot;
+
     return {
         name: "scripts",
+        sourceRoot: sourceRoot,
+        filesRoot: filesRoot,
         initialize: function () {
-            runtime.audit("mycompanymoduleloaded", "Scripts module loaded.");
+            if (!fs.existsSync(sourceRoot)) {
+                throw new Error("My Scripts source files are missing. Run npm run sync:files.");
+            }
+            runtime.audit("mycompanymoduleloaded", "Scripts module loaded from " + sourceRoot + ".");
         }
     };
 };
