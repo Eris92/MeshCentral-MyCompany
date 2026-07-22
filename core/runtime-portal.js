f8bb068a5d048a3be738c61f8ea9f8d7d5b73b72
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-22 08:30:53 +0200

    chore: report MyCompany runtime 1.5.4

--- core/runtime-portal.js
+++ core/runtime-portal.js
@@ -4,7 +4,7 @@ var shared = require("./shared.js");
 var baseFactory = require("./runtime.js");
 var portalFactory = require("../modules/Portal/index-safe.js");
 
-var VERSION = "1.5.3";
+var VERSION = "1.5.4";
 var PORTAL_DEFAULTS = {
     enabled: false,
     defaultView: "overview",
