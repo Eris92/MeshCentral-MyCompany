7732a7c178d1c46897c65f84e7877dfd9d2bdd03
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-23 23:38:13 +0200

    refactor(server): remove legacy Approval Center backend path

--- modules/ApprovalCenter/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-// Compatibility shim for installations and extensions that still import the
-// historical backend path. New code must use server/modules/approval-center.
-module.exports = require("../../server/modules/approval-center/index.js");
