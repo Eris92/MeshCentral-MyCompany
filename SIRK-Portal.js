730b0a2ceb3e7b6614480b285e5e639985cae694
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-23 23:37:15 +0200

    refactor(entry): keep one canonical SIRK Portal export

--- SIRK-Portal.js
+++ SIRK-Portal.js
@@ -2,14 +2,6 @@
 
 var implementation = require("./plugin-main-standalone.js");
 
-function create(parent, shortName) {
-    return implementation.createPlugin(parent, shortName || "SIRK-Portal");
-}
-
-module.exports.create = create;
 module.exports["SIRK-Portal"] = function (parent) {
-    return create(parent, "SIRK-Portal");
-};
-module.exports.SIRKPortal = function (parent) {
-    return create(parent, "SIRK-Portal");
+    return implementation.createPlugin(parent, "SIRK-Portal");
 };
