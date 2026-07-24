ae000ee946621f5e07bd30513fd297398124b16b
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-24 09:32:08 +0200

    fix: prevent self-redirect on slash portal route

--- SIRKPortal.js
+++ SIRKPortal.js
@@ -1,7 +1,17 @@
 "use strict";
 
 var implementation = require("./plugin-main-standalone.js");
+var routeCompat = require("./server/core/express-route-compat.js");
 
 module.exports.SIRKPortal = function (parent) {
-    return implementation.createPlugin(parent, "SIRKPortal");
+    var plugin = implementation.createPlugin(parent, "SIRKPortal");
+    var setupHttpHandlers = plugin.hook_setupHttpHandlers;
+
+    plugin.hook_setupHttpHandlers = function (webserver, meshServer) {
+        return routeCompat.withExactPortalRedirect(webserver && webserver.app, function () {
+            return setupHttpHandlers.call(plugin, webserver, meshServer);
+        });
+    };
+
+    return plugin;
 };
